4542ac07145da9e75cb52c070ea885fbf7480f80
MatthewsM2 <matthew17773@gmail.com>
2024-05-18 14:25:54 +0530

    Back btn added

--- Menu.gd
+++ Menu.gd
@@ -6,6 +6,8 @@ extends Control
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://game_page/game_scence.tscn")
 
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
 
 func _on_options_pressed():
 	pass # Replace with function body.
--- game_page/game.gd
+++ game_page/game.gd
@@ -3,3 +3,8 @@ func _on_back_pressed():
 	get_tree().change_scene_to_file("res://menu.tscn")
 
 
+
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
+	#pass # Replace with function body.
